8c47b8aad105fbad72d1da40aa912e00203c0135
GhitCodZonne <157253634+GhitCodZonne@users.noreply.github.com>
2024-01-26 15:05:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 from keep_alive import keep_alive
 
 # Créez une instance de client avec votre propre token de bot et votre nom d'utilisateur
-app = Client("my_account", bot_token="5794110227:AAHYa4vaeUfQr7LoYKWin0_tX2Gu_sccAco", api_id="21648908", api_hash="a6f834b1a8f86046078f05bfe34c0a5f")
+app = Client("my_account", bot_token="6096646052:AAHneohzQ1etEvqy7irhzQBm-djSja9G6UY", api_id="21648908", api_hash="a6f834b1a8f86046078f05bfe34c0a5f")
 Admin_id = 6217351762
 
 # Créez un sémaphore avec une limite de 1
